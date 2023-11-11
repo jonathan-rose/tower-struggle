a47e4fd32fc10f2ca9d41d78a107a4f7b683e199
Jonathan Rose <jonathan_rose@outlook.com>
2023-11-11 17:15:30 +0000

    draw-tower-outline function implemented that draws a dashed outline to the screen.

--- src/tower_struggle/scenes/level_01.clj
+++ src/tower_struggle/scenes/level_01.clj
@@ -11,11 +11,48 @@
   []
   [(t/tetromino [300 800])])
 
+(defn tower-outline
+  [x y w h]
+  (let [tower-width w
+        tower-height h
+        coords {:tl  [x y]
+                :tr  [(+ x tower-width) y]
+                :bl  [x (+ y tower-height)]
+                :br  [(+ x tower-width) (+ y tower-height)]}
+        sides {:left   (concat (coords :tl) (coords :bl))
+               :right  (concat (coords :tr) (coords :br))
+               :top    (concat (coords :tl) (coords :tr))
+               :bottom (concat (coords :br) (coords :bl))}]
+    sides))
+
+(defn draw-tower-outline
+  "Draws a rectangle of dashed lines using Quil line primitives."
+  [x y w h]
+  (q/stroke common/black)
+  (let [o (tower-outline x y w h)
+        dash-length 10]
+    (doseq [[key value] o]
+      (let [distance (apply q/dist value)
+            dash-count (int (/ distance dash-length))
+            x1 (first value)
+            y1 (second value)
+            x2 (nth value 2)
+            y2 (nth value 3)
+            x-inc (/ (- x2 x1) dash-count)
+            y-inc (/ (- y2 y1) dash-count)]
+        (doseq [i (range 0 dash-count 2)]
+          (let [x-start (+ x1 (* i x-inc))
+                y-start (+ y1 (* i y-inc))
+                x-end (+ x1 (* (inc i) x-inc))
+                y-end (+ y1 (* (inc i) y-inc))]
+            (q/line x-start y-start x-end y-end)))))))
+
 (defn draw-level-01
   "Called each frame, draws the current scene to the screen"
   [state]
   (qpu/background common/blue)
-  (qpsprite/draw-scene-sprites state))
+  (qpsprite/draw-scene-sprites state)
+  (draw-tower-outline 50 50 100 400))
 
 (defn transfer-locked-tetrominos
   "Move tetrominos which have just been locked in to the longer term
